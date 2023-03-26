76c1f595d78082122472713789a95d075e9d246f
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 14:48:48 +0200

    1000-ig minden szám négyzete

---
+++ 017. szamoknegyzeteezerig/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _017.szamoknegyzeteezerig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1001; i++)
+
+            {
+                Console.WriteLine("A(z) " + i + " négyzete: " + i * i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
